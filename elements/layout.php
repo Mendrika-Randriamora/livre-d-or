4bb932f7dde9690029b97f9faa9f9a17957db298
Mendrika-Randriamora <wanimirana@gmail.com>
2025-07-25 11:44:27 +0300

    Merge commit '95f13ed025df78cc5cd1e3167054bbf936229993'

--- elements/layout.php
+++ elements/layout.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./../public/css/bootstrap.min.css">
     <title><?= $title ?? "Livre d'or" ?></title>
 </head>
 <body>
     
     <div class="container">
-        <?= $page_content ?? "" ?>
+        <?= $content ?? "" ?>
     </div>
 
 </body>
